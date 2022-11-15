cd374005a538e4715b5a13118c4ddce8057be6a7
jonmelo28 <103691470+jonmelo28@users.noreply.github.com>
2022-11-15 01:00:03 -0300

    Add files via upload

--- src/components/footer.php
+++ src/components/footer.php
@@ -1,7 +1,7 @@
 <footer class="px-4 divide-y text-gray-800  bottom-0 w-full mt-12">
     <div class="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div class="grid place-items-center lg:w-1/3">
-            <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <a href="../" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                 <span class="w-5 h-5 text-white p-2 bg-gradient-to-r from-green-500 to-lime-400 rounded-full">
                 </span>
                 <span class="ml-3 text-xl">Casa Pia</span>
@@ -12,7 +12,7 @@
                 <h3 class="tracking-wide uppercase text-gray-900">Posts</h3>
                 <ul class="space-y-1">
                     <li>
-                        <a rel="noopener noreferrer" href="#">Dicas</a>
+                        <a rel="noopener noreferrer" href="../">Dicas</a>
                     </li>
                 </ul>
             </div>
@@ -20,14 +20,14 @@
                 <h3 class="tracking-wide uppercase text-gray-900">Organização</h3>
                 <ul class="space-y-1">
                     <li>
-                        <a rel="noopener noreferrer" href="#">Sobre Nós</a>
+                        <a rel="noopener noreferrer" href="../">Sobre Nós</a>
                     </li>
-                    <li>
-                        <a rel="noopener noreferrer" href="#">Contato</a>
-                    </li>
+                        <!-- <li>
+                            <a rel="noopener noreferrer" href="../">Contato</a>
+                        </li> -->
                 </ul>
             </div>
-            <div class="space-y-3">
+            <!-- <div class="space-y-3">
                 <div class="uppercase text-gray-900">Mídias Sociais</div>
                 <div class="flex justify-start space-x-3">
                     <a rel="noopener noreferrer" href="#" title="Facebook" class="flex items-center p-1">
@@ -40,7 +40,7 @@
 
                     </a>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
     <div class="py-6 text-sm text-center text-gray-600">© 2022 Casa Pia. Todos os direitos reservados.</div>
